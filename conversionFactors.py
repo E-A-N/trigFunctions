e95a7bbc92cd0a7a58dccf7cff960d5c9ec57668
Cyborg_Ean <ean.director@gmail.com>
2017-03-08 09:19:07 -0800

    Adding definitions to Python dictionary

--- conversionFactors.py
+++ conversionFactors.py
@@ -3,7 +3,7 @@ def degreeDepth(type ,unit):
     minutes = unit[1]
     seconds = unit[2]
     newUnit = 0
-    
+
     if type =="degree":
       for i in range(degrees):
         newUnit += 1
@@ -12,29 +12,29 @@ def degreeDepth(type ,unit):
       return 'minute'
     elif type == "seconds":
       return 'seconds'
-      
-      
+
+
 
 def secondReduce(degree=0,minute=0,second=0):
     conversion = 0
     while degree > 0:
-        minute += 60
+        minute += 60r
         degree -= 1
     while minute > 0:
         second += 60
         minute -= 1
-        
+
     conversion += second
     return conversion
-    
+
 def minuteReduce(degree=0,minute=0,second=0):
-    conversion = 0 
+    conversion = 0
     while degree > 0:
         minute += 60
-        
+
     while second > 60:
         minute += 1
         second -= 60
-        
+
     conversion += minute
-    return conversion    
+    return conversion
---
+++ memoryGame/definitions.py
@@ -0,0 +1,23 @@
+trig = {
+    "hypotenuse": {
+        "formula": "The square root of x^2 + y^2",
+        "definition": "The Longest side of a right triangle, opposite of the right angle"
+    }
+    "sine": {
+        "formula": "Math formula,
+        "definition": "(sine) The Trigonometric functino that is equal to the ratio of the sid e opposite a given angle (in a right triangle) to the hypotenuse"
+    }
+    "cosine": {
+        "formula": "Math formula,
+        "definition": "A function of the adjacent end of a triangle over hypotenuse"
+    }
+    "complement": {
+        "formula": "Math formula,
+        "definition": "Word definition"
+    }
+    "trigword": {
+        "formula": "Math formula,
+        "definition": "Word definition"
+    }
+
+}
