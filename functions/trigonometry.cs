632ff3b2998e5664f14bc4c03437f9130ffce6de
Cyborg_Ean <ean.director@gmail.com>
2017-03-07 22:45:25 -0800

    Working on Triangle field members

--- functions/trigonometry.cs
+++ functions/trigonometry.cs
@@ -8,6 +8,26 @@ namespace ConsoleApplication{
         private const int angle3;
         private const int oppositeLength;
         private const int adjacentLength;
+        private const int valid;
+
+        public void setAngles(int a1, int a2, int a3){
+            this.angle1 = a1;
+            this.angle2 = a2;
+            this.angle3 = a3;
+        }
+
+        private bool validateAngles(){
+            bool validate;
+            int area = this.angle1 + this.angle2 + this.angle1;
+
+            if (area == 180){
+                validate = true;
+            }
+            else{
+                validate = false;
+            }
+            return validate;
+        }
 
     }
 
