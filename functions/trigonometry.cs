9d82911d169d3572ac5ffc567d2ca8e48a48f218
Cyborg_Ean <ean.director@gmail.com>
2017-03-07 22:33:26 -0800

    Establishing triangle class

---
+++ functions/trigonometry.cs
@@ -0,0 +1,28 @@
+using System;
+namespace ConsoleApplication{
+
+    public class Triangle{
+
+        private const int angle1;
+        private const int angle2;
+        private const int angle3;
+        private const int oppositeLength;
+        private const int adjacentLength;
+
+    }
+
+    public class TrigFuncs {
+        public static int sine(int x, int y){
+            //hypotenuse
+            int r = (x * x) + (y * y);
+
+            return r;
+
+        }
+
+
+        public static void Main(){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
