983a796b9e5ff9a284a777dc839b67155f904f96
Cyborg_Ean <ean.director@gmail.com>
2017-03-08 09:40:41 -0800

    Adding name field!

--- memoryGame/definitions.py
+++ memoryGame/definitions.py
@@ -1,23 +1,38 @@
 trig = {
     "hypotenuse": {
+        "name":"hypotenuse",
         "formula": "The square root of x^2 + y^2",
         "definition": "The Longest side of a right triangle, opposite of the right angle"
-    }
+    },
     "sine": {
-        "formula": "Math formula,
+        "name":"sine",
+        "formula": "Math formula",
         "definition": "(sine) The Trigonometric functino that is equal to the ratio of the sid e opposite a given angle (in a right triangle) to the hypotenuse"
-    }
+    },
     "cosine": {
-        "formula": "Math formula,
+        "name":"cosine",
+        "formula": "Math formula",
         "definition": "A function of the adjacent end of a triangle over hypotenuse"
-    }
+    },
     "complement": {
-        "formula": "Math formula,
-        "definition": "Word definition"
-    }
+        "name":"complement",
+        "formula": "Math formula",
+        "definition": "The difference of two angles angles whose sume is 90 degrees"
+    },
+    "supplement": {
+        "formula": "Math formula",
+        "definition": "The difference of two angles whose sum is 180 degrees"
+    },
+    "degrees": {
+        "formula": "Math formula",
+        "definition": "A unit used to measure an angle"
+    },
+    "minutes": {
+        "formula": "Math formula",
+        "definition": "A unit for measuring angles more precise than degree units, 60 minutes is equal to 1 degrees"
+    },
     "trigword": {
-        "formula": "Math formula,
+        "formula": "Math formula",
         "definition": "Word definition"
-    }
-
+    },
 }
